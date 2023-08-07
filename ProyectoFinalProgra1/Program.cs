92c5d5f028d2a7703ca131404597176e2f563d06
Daniel Sequeira <danielsequeira22@gmail.com>
2023-08-06 23:04:30 -0600

    Corrección Clases

--- ProyectoFinalProgra1/Program.cs
+++ ProyectoFinalProgra1/Program.cs
@@ -6,7 +6,7 @@ namespace ProyectoFinalProgra1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("MENU PRINCIPAL");
         }
     }
 }
