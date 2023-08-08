4f8daffb740536031726b399a58281cf28783f57
Daniel Sequeira <danielsequeira22@gmail.com>
2023-08-07 22:38:18 -0600

    estructura menu principal y txt

--- ProyectoFinalProgra1/Program.cs
+++ ProyectoFinalProgra1/Program.cs
@@ -7,6 +7,25 @@ namespace ProyectoFinalProgra1
         static void Main(string[] args)
         {
             Console.WriteLine("MENU PRINCIPAL");
+
+            
+            Console.WriteLine(" 1 VER SALARIOS  ");
+            Console.WriteLine(" 2 AGREGAR EMPLEADOS ");
+
+            int opcion;
+
+            switch (opcion)
+            {
+                case (1):
+
+
+                    break;
+
+                case (2):
+
+
+                    break;
+            }
         }
     }
 }
