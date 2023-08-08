07e3a5a4037d8995c8b33dcce3382ddfbfcf3330
Fabian Arredondo <fabianarredondo365@gmail.com>
2023-08-07 23:53:22 -0600

    Comento los metodos de validacion

--- ProyectoFinalProgra1/Validaciones.cs
+++ ProyectoFinalProgra1/Validaciones.cs
@@ -7,6 +7,7 @@ namespace ProyectoFinalProgra1
 {
     class Validaciones
     {
+<<<<<<< HEAD
         public static bool ValidarCedula(string cedula)
         {
             string patron = @"^\d{9}$";
@@ -26,6 +27,22 @@ namespace ProyectoFinalProgra1
     }  
 
     
+=======
+        public static bool ValidarCedula(string cedula) // El metodo que valida si el usuario ingresa de manera correcta su cedula
+        {
+            string patron = @"^\d{9}$";//Define una variable llamada patron que contiene una expresión regular (cadena de 9 digitos)
+            return Regex.IsMatch(cedula, patron);//Método IsMatch de la clase Regex valida si la cadena de texto almacenada en la variable cedula 
+        }
+
+        public static bool ValidarEmail(string email) // El metodo que valida si el usuario ingresa de manera correcta su correo
+        {
+            string patron = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"; //La expresión regular @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"valida si cadena coincide con formato dirección email.
+            return Regex.IsMatch(email, patron);//verificar si la cadena de texto almacenada en la variable email coincide con el patrón definido anteriormente.
+        }
+
+
+    }
+>>>>>>> rama-prueba
 }
 
 
