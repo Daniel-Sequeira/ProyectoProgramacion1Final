edacd263e5f96e1cc5a3345be69d189d1b0407a3
Daniel Sequeira <danielsequeira22@gmail.com>
2023-08-08 22:22:24 -0600

    Actulizacion cambios entrantes

--- ProyectoFinalProgra1/EscribirDatos.cs
+++ ProyectoFinalProgra1/EscribirDatos.cs
@@ -6,6 +6,29 @@ namespace ProyectoFinalProgra1
 {
     public class EscribirDatos
     {
+        public static void AgregarEmpleado()
+        {
+            Console.WriteLine("Ingrese el nombre del empleado: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el salario del empleado: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Datos del empleado:");
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Salario: " + salario);
+
+            Console.WriteLine("Desea guardar los datos? (S/N)");
+            string opcion = Console.ReadLine();
+            if (opcion.ToUpper() == "S")
+            {
+                Console.WriteLine("Datos guardados exitosamente");
+            }
+            else
+            {
+                Console.WriteLine("Los datos no han sido guardados");
+            }
+        }
 
     }
 }
