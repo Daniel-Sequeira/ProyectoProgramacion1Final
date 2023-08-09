d90a91d4c67387e57a7a78f4899bdc3378d529eb
Fabian Arredondo <fabianarredondo365@gmail.com>
2023-08-08 21:15:42 -0600

    Comienzo el metodo para agregar empleados

--- ProyectoFinalProgra1/EscribirDatos.cs
+++ ProyectoFinalProgra1/EscribirDatos.cs
@@ -6,6 +6,28 @@ namespace ProyectoFinalProgra1
 {
     public class EscribirDatos
     {
-        //Crear un metodo que solicite los datos del nuevo empleado y muestre la opcion de guardar o salir
+        public static void AgregarEmpleado()
+        {
+            Console.WriteLine("Ingrese el nombre del empleado: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el salario del empleado: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Datos del empleado:");
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Salario: " + salario);
+
+            Console.WriteLine("Desea guardar los datos? (S/N)");
+            string opcion = Console.ReadLine();
+            if (opcion.ToUpper() == "S")
+            {
+                Console.WriteLine("Datos guardados exitosamente");
+            }
+            else
+            {
+                Console.WriteLine("Los datos no han sido guardados");
+            }
+        }
     }
 }
