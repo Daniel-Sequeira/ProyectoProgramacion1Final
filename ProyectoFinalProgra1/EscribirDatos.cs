3c826826c8f3e4b95e36e6b21c861e1605ff8215
Daniel Sequeira <danielsequeira22@gmail.com>
2023-08-18 14:02:12 -0600

    Guardado para presentar trabajo

--- ProyectoFinalProgra1/EscribirDatos.cs
+++ ProyectoFinalProgra1/EscribirDatos.cs
@@ -36,8 +36,8 @@ namespace ProyectoFinalProgra1
         /// <param name="rebajos"></param>
         public void AgregarEmpleado(string cedula, string nombre, string email, string id, string profesion, double salarioHora, double horasTrabajadas, double rebajos)
         {
-            string empleadoInfo = $"{cedula} {nombre} {email}";
-            string planillaInfo = $"{id} {profesion} {salarioHora} {horasTrabajadas} {rebajos}%";
+            string empleadoInfo = $"\n{cedula} {nombre} {email}";
+            string planillaInfo = $"\n{id} {profesion} {salarioHora} {horasTrabajadas} {rebajos}%";
             using (StreamWriter swEmp = new StreamWriter(pathEmp, true))
             {
                 swEmp.WriteLine(empleadoInfo);
